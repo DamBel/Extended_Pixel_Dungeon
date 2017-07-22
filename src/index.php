5d5382433d2dd7ccd215c8b05bd626f8f077fd81
Damien <newvache@gmail.com>
2017-07-22 17:37:11 +0200

    Moved index.php to src

---
+++ src/index.php
@@ -0,0 +1,34 @@
+<?php
+
+	session_start();
+
+?>
+
+<body>
+
+<?php
+
+if (isset($_GET["name"])){
+
+	switch ($_GET["name"]){
+
+		case "Home" :
+		case "404" :
+			require_once("./view/homepage.html.php");
+			break;
+
+		case "Features" : require_once("./view/features.html.php"); break;
+		case "Wiki" : require_once("./view/wiki_home.html.php"); break;
+		case "About" : require_once("./view/about.html.php"); break;
+		case "Development" : require_once("./view/development.html.php"); break;
+		default: require_once("./view/404.html.php"); break;
+	}
+} else {
+	require_once("./src/view/homepage.html.php");
+
+}
+
+?>
+
+</body>
+</html>
